f7829919ad357f8436097b26425f7d0b07f15286
Azizul <arafinazizul@gmail.com>
2023-09-15 21:44:40 +0600

    Remove unused code and files

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,48 @@ import Header from "./Component/Header/Header"
 
 
 function App () {
-  
-  const [ addCourse, setaddCourse ] = useState( [] );
-  const [ creditHr, setCreditHr ] = useState(0);
-  console.log();
-  
-  const handleAddCourse = ( course ) => {
-    const isSelected = addCourse.find( item => item.id == course.id )
-    if ( isSelected ) {
-      alert('all ready added')
 
-    } else {
-      const newAddCourse = [...addCourse,course];
-    setaddCourse( newAddCourse );
-    }
+  const [ addCourse, setaddCourse ] = useState( [] );
+  const [ creditHr, setCreditHr ] = useState( 0 );
+  const [ remainingHr, setRemainingHr ] = useState( 20 );
+  const [ price, setPrice ] = useState( 0 );
+
+
+  const handleAddCourse = ( course ) => {
     
-    const newCreditHr = ( creditHr + course.course_credit );
+    const isSelected = addCourse.find( item => item.id == course.id )
+    if ( isSelected || remainingHr <= 1) {
+      return alert( 'You All Ready Added The Course Or Your Credit Limit is Crossed' )
+
+    }
+    else {
+      const newAddCourse = [ ...addCourse, course ];
+      setaddCourse( newAddCourse );
+      const newCreditHr = ( creditHr + course.course_credit );
     setCreditHr( newCreditHr )
-    console.log(creditHr);
+    const newRemaining = ( remainingHr - course.course_credit );
+      setRemainingHr( newRemaining );
+      const newPrice = ( price + course.course_price );
+      setPrice( newPrice );
+    }
+
   }
-  
+
 
   return (
     <div className="bg-[#F3F3F3]">
       <Header></Header>
       <div className="max-w-screen-xl mx-auto flex gap-5 ">
         <div className="w-4/5 mx-auto">
-        <Blogs handleAddCourse={handleAddCourse}  ></Blogs>
+          <Blogs handleAddCourse={ handleAddCourse }  ></Blogs>
         </div>
         <div className="">
-          <AddCourse  addCourse={addCourse}></AddCourse>
+          <AddCourse addCourse={ addCourse } creditHr={ creditHr } remainingHr={ remainingHr } price={price}></AddCourse>
         </div>
-        
-      
+
+
       </div>
-      
+
     </div>
   )
 }
--- src/Component/AddCourse/AddCourse.jsx
+++ src/Component/AddCourse/AddCourse.jsx
@@ -1,19 +1,25 @@
 /* eslint-disable react/prop-types */
 
-const AddCourse = ( { addCourse } ) => {
+const AddCourse = ( { addCourse,creditHr,remainingHr,price } ) => {
 
     return (
         <div className="p-4 w-96 bg-white">
-            <h1 className="text-[#2F80ED] text-lg font-semibold ">Credit Hour Remaining{ addCourse.course_credit }</h1>
+            <div className="border-b-2 pb-4">
+            <h1 className="text-[#2F80ED] text-lg font-semibold ">Credit Hour Remaining { creditHr } hr</h1>
             <h4 className="text-lg font-semibold">Course Name</h4>
             {
-                addCourse.map( selectCourse => {
+                addCourse.map( (selectCourse,idx) => {
                     return <ol key={ selectCourse.id }>
-                        <li>{ selectCourse.course_name }</li>
+                        <li className="text-[#1C1B1B99]">{idx +1 }. { selectCourse.course_name }</li>
+                        
                     </ol>
-
+                    
                 } )
             }
+            </div>
+            <div className="border-b-2 pb-4 text-base font-semibold text-[#1C1B1BCC]">Total Credit Hour: { remainingHr }</div>
+            <div className="border-b-2 pb-4 text-lg text-[#1C1B1BCC] font-semibold">Total Price:{price }</div>
+            
 
         </div>
     );
